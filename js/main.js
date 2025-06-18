570b72cefc27eeb68c28072291578f2bea5a848c
Saad <154870199+saad-726@users.noreply.github.com>
2025-06-18 21:48:36 +0500

    JS Level 3 completed --JS

--- js/main.js
+++ js/main.js
@@ -1,43 +1,68 @@
-const profile = {
-    pName:"x._saadi_.x",
-    post:128,
-    followers:320,
-    following:1,
-    isFollowed: false
+// const profile = {
+//     pName:"x._saadi_.x",
+//     post:128,
+//     followers:320,
+//     following:1,
+//     isFollowed: false
 
 
-};
-console.log(profile)
+// };
+// console.log(profile)
 
-let num=prompt("Enter a number");
-if (num%5==0)
-{
-    console.log("It is a multiple of 5")
-}
-else{
-    console.log("Not a multiple of 5")
+// let num=prompt("Enter a number");
+// if (num%5==0)
+// {
+//     console.log("It is a multiple of 5")
+// }
+// else{
+//     console.log("Not a multiple of 5")
+// }
+
+// let score=prompt("Enter your exam score :")
+// if (score>=80 && score<=100){
+//     console.log("Grade A");
+// }
+// else if (score>=70 && score<80)
+// {
+//     console.log("Grade B");
+// }
+// else if (score>=60 && score<70)
+// {
+//     console.log("Grade C");
+// }
+// else if (score>=50 && score<60)
+// {
+//     console.log("Grade D");
+// }
+// else if (score>=0 && score<50)
+// {
+//     console.log("Grade F");
+// }
+// else{
+//     console.log("Invalid input");
+// }
+
+// for (i=0;i<=100;i+=2)
+// {
+    
+//         console.log(i);
+    
+// }
+
+//JS Level 3
+//q1
+// i=0;
+// while (i<=100){
+//     console.log(i);
+//     i+=2;
+// }
+//q2
+let gameNum=25;
+let guess=prompt("Guess a number between 1 to 25");
+guess = Number(guess);
+while (gameNum!==guess){
+guess=prompt("Try again with a new guess :");
+
 }
 
-let score=prompt("Enter your exam score :")
-if (score>=80 && score<=100){
-    console.log("Grade A");
-}
-else if (score>=70 && score<80)
-{
-    console.log("Grade B");
-}
-else if (score>=60 && score<70)
-{
-    console.log("Grade C");
-}
-else if (score>=50 && score<60)
-{
-    console.log("Grade D");
-}
-else if (score>=0 && score<50)
-{
-    console.log("Grade F");
-}
-else{
-    console.log("Invalid input");
-}
+    console.log("You have guessed that right!");
