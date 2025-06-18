d01ef21d83eb95308b66326111354d8cc558ff52
Saad <154870199+saad-726@users.noreply.github.com>
2025-06-18 18:11:36 +0500

    JS level 2 completed --JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,43 @@
-const Product = {
-    pName : "Ball Pen",
-    rating : 4.5,
-    onDeal: true,
-    Price : 270,
-    Discount : 5,
+const profile = {
+    pName:"x._saadi_.x",
+    post:128,
+    followers:320,
+    following:1,
+    isFollowed: false
+
 
 };
+console.log(profile)
+
+let num=prompt("Enter a number");
+if (num%5==0)
+{
+    console.log("It is a multiple of 5")
+}
+else{
+    console.log("Not a multiple of 5")
+}
+
+let score=prompt("Enter your exam score :")
+if (score>=80 && score<=100){
+    console.log("Grade A");
+}
+else if (score>=70 && score<80)
+{
+    console.log("Grade B");
+}
+else if (score>=60 && score<70)
+{
+    console.log("Grade C");
+}
+else if (score>=50 && score<60)
+{
+    console.log("Grade D");
+}
+else if (score>=0 && score<50)
+{
+    console.log("Grade F");
+}
+else{
+    console.log("Invalid input");
+}
